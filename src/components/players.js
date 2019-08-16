e313d4dc3e32a88a587956119fc58c5d5beee73a
sam11385 <samluedke@gmail.com>
2019-08-16 10:41:46 -0400

    Add most repped nation to UI.

--- src/components/players.js
+++ src/components/players.js
@@ -3,23 +3,46 @@ import './Players.scss'
 
 const Players = ({ players }) => {
 
-  // Instead of using a loop like we used to do
-  // just map what you want
   function getGoals(){
-    return players.map(player => player.goals);
+    return players.map(player => player.goals)
   }
   
   function getMostGoals(){
-    return Math.max(0, ...getGoals());
+    return Math.max(0, ...getGoals())
   }
 
   function getApps() {
-    return players.map(player => player.appstotal);
+    return players.map(player => player.appstotal)
   }
 
   function getMostApps() {
     return Math.max(0, ...getApps())
   }
+
+  function getNations() {
+    return players.map(player => player.nationality)
+  }
+
+  function mostNations() {
+    const allNations = getNations();
+    let mostFrequent = 1;
+    let m = 0;
+    let nation;
+    for (let i=0; i<allNations.length; i++) {
+      for (let j=i; j<allNations.length; j++) {
+        if (allNations[i] === allNations[j]) {
+          m++;
+        }
+        if (mostFrequent<m) {
+          mostFrequent=m; 
+          nation = allNations[i];
+        }
+      }
+      m=0;
+    }
+    return `${mostFrequent} players from ${nation} have over 100 appearances!`;
+  }
+
 
 
   return (
@@ -28,7 +51,7 @@ const Players = ({ players }) => {
 
         <h2>Most Goals: {getMostGoals()}</h2>
         <h2>Most Appearances: {getMostApps()}</h2>
-        
+        <h2>Most represented country: {mostNations()}</h2>
         <div className="cards">
             {players.map((player) => 
                 <div className="card">
