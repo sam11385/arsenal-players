702396ab2a42d40843a763e9608821e9b6a31a93
sam11385 <samluedke@gmail.com>
2019-08-15 21:37:21 -0400

    update some CSS and try to do more with the data.

--- src/components/players.js
+++ src/components/players.js
@@ -2,13 +2,27 @@ import React from 'react'
 import './Players.scss'
 
 const Players = ({ players }) => {
+  
+  players.map((player) => {
+    let scot = 0;
+    if(player.nationality === 'Scotland') {
+      return console.log(player.nationality)
+    }
+  })
+
+  
+
   return (
     <div>
         <h1>Player List</h1>
         
         <div className="cards">
             {players.map((player) => 
-                <div className="card">{player.player}, {player.nationality}, {player.club_career}</div>
+                <div className="card">
+                  <div><span>Player:</span> {player.player}</div> 
+                  <div><span>Nationality:</span> {player.nationality}</div>
+                  <div><span>Time at Club:</span> {player.club_career}</div>
+                </div>
             )}
         </div>
         
